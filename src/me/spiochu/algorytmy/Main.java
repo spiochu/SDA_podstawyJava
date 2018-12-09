455d84ef2ff5b1cb266757b6ba87bc35ceab8c77
Tomasz <spiochpl@gmail.com>
2018-12-09 12:18:11 +0100

    Stos and Table class add

--- src/me/spiochu/algorytmy/Main.java
+++ src/me/spiochu/algorytmy/Main.java
@@ -1,8 +1,67 @@
 package me.spiochu.algorytmy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        wywolajStosTab();
+
+
+    }
+
+
+    public static void wywolajStosTab(){
+        Scanner scaner = new Scanner(System.in);
+        int tab[];
+        int rozmiar;
+        StosTab stosTab;
+        System.out.println("Podaj rozmiar tablicy:");
+        rozmiar = scaner.nextInt();
+        tab = new int[rozmiar];
+        stosTab = new StosTab(rozmiar);
+        System.out.println("Podaj elementy tablicy:");
+        while (true){
+            System.out.println("1- push\n" +
+                                "2 - pop\n"+
+                                "3 - peek\n"+
+                                "4 - show\n"+
+                                "0 - wyjscie");
+            int operacja = scaner.nextInt();
+
+            switch (operacja){
+                case 0:
+                    return;
+                case 1:
+                    System.out.println("Podaj wartosc na stos");
+                    int w = scaner.nextInt();
+                    try{
+                        stosTab.push(w);
+                    }catch (ArrayIndexOutOfBoundsException e){
+                        System.out.println("!!TAblica pełna!!");
+                    }
+
+                    break;
+                case 2:
+                    try {
+                        System.out.println("POP: " + stosTab.pop());
+                    } catch (ArrayIndexOutOfBoundsException e){
+                        System.out.println("!!Pusta tablica!!");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Peek: "+ stosTab.peek());
+                    break;
+                case 4:
+                    stosTab.show();
+                    break;
+            }
+        }
+//        for (int i = 0; i < rozmiar; i++) {
+//            tab[i] = scaner.nextInt();
+//        }
+
+
     }
 }
--- src/me/spiochu/algorytmy/StosTab.java
+++ src/me/spiochu/algorytmy/StosTab.java
@@ -9,11 +9,35 @@ public class StosTab {
         top = -1;
     }
 
-    public void puch(int wartosc){
+    public void push(int wartosc) throws ArrayIndexOutOfBoundsException {
+        if (isFull()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
         tab[++top] = wartosc;
     }
 
-    public int pop(){
+    public int pop() throws ArrayIndexOutOfBoundsException {
+        if (isEmpty()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
         return tab[top--];
     }
+
+    public boolean isEmpty(){
+        return top == -1;
+    }
+    public boolean isFull(){
+        return top == tab.length-1;
+    }
+
+    public int peek(){
+        return tab[top];
+    }
+    public void show(){
+        for (int i = 0; i <= top; i++) {
+            System.out.print(tab[i]+ " ");
+        }
+        System.out.println();
+    }
 }
+
